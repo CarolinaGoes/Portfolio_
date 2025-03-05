773d1f5e754f3af3e335e6aa6cfbe14b593e1c25
CarolinaGoes <carolinarocha.89@hotmail.com>
2025-03-04 21:47:52 -0300

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,49 +1,31 @@
-function abrirPopup() {
-    document.getElementById("janela").style.display = "block";
-}
-
-function fecharPopup() {
-    document.getElementById("janela").style.display = "none";
-    document.querySelector("main").style.display = "block"; // Exibe o conteúdo do main
-}
-
-function enviar() {
-    const nome = document.getElementById("inputNome").value;
+function backgroundImage() {
     const mensagem = document.getElementById("mensagem");
     const saudacao = document.getElementById("saudação");
-    const body = document.body; // Referência ao corpo da página
+    const body = document.body; 
 
-    if (nome.trim() !== "") {
-        const agora = new Date(); 
-        const hora = agora.getHours(); 
-        let saudacaoTexto;
-        let backgroundImage;
+    const agora = new Date(); 
+    const hora = agora.getHours(); 
+    let saudacaoTexto;
+    let backgroundImage;
 
-        // Condições para definir a saudação e a imagem de fundo
-        if (hora >= 6 && hora < 12) {
-            saudacaoTexto = "Bom dia, " + nome + "!";
-            backgroundImage = "url('assets/d1.jpg')"; 
-        } else if (hora >= 12 && hora < 18) {
-            saudacaoTexto = "Boa tarde, " + nome + "!";
-            backgroundImage = "url('assets/t1.jpg')"; 
-        } else if (hora >= 18 && hora < 24) {
-            saudacaoTexto = "Boa noite, " + nome + "!";
-            backgroundImage = "url('assets/n1.jpg')"; 
-        } else {
-            saudacaoTexto = "Boa madrugada, " + nome + "!";
-            backgroundImage = "url('assets/m1.jpg')"; 
-        }
-
-        saudacao.innerHTML = saudacaoTexto; 
-        body.style.backgroundImage = backgroundImage; 
-        
-        fecharPopup();
-        
+    // Definindo a saudação e a imagem de fundo com base na hora
+    if (hora >= 6 && hora < 12) {
+        saudacaoTexto = "Bom dia!";
+        backgroundImage = "url('assets/d1.jpg')"; 
+    } else if (hora >= 12 && hora < 18) {
+        saudacaoTexto = "Boa tarde!";
+        backgroundImage = "url('assets/t1.jpg')"; 
+    } else if (hora >= 18 && hora < 24) {
+        saudacaoTexto = "Boa noite!";
+        backgroundImage = "url('assets/n1.jpg')"; 
     } else {
-        mensagem.innerHTML = "Por favor, digite seu nome!"; 
+        saudacaoTexto = "Boa madrugada!";
+        backgroundImage = "url('assets/m1.jpg')"; 
     }
+    
+    saudacao.innerHTML = saudacaoTexto; 
+    body.style.backgroundImage = backgroundImage; 
 }
 
-window.onload = function() {
-    abrirPopup();
-}
+// Chama a função quando a página é carregada
+window.onload = backgroundImage;
